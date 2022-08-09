019970cff342dc46042094da533f20de517f8908
Shinya Oguri <36407060+shinyaoguri@users.noreply.github.com>
2022-08-09 13:47:08 +0900

    Update arm64_mac_setup.sh

--- arm64_mac_setup.sh
+++ arm64_mac_setup.sh
@@ -7,7 +7,7 @@ echo -e "-----\nXcodeの存在確認"
 if type "xcode-select" >/dev/null 2>&1; then
   echo -e "-> ✅ Xcode already exist"
 else
-  echo -e ">>> Xcode was not exists\n>>> Please install Xcode from AppStore."
+  echo -e ">>>🙅 Xcode was not exists\n>>> Please install Xcode from AppStore."
   return 2> /dev/null
   exit
 fi
@@ -19,7 +19,7 @@ echo -e "-----\nHomebrewの存在確認"
 if type "brew" >/dev/null 2>&1; then
   echo -e "-> ✅ brew already exist"
 else
-  echo -e ">>> Homebrew was not exist"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo -e ">>>🙅 Homebrew was not exist\n Please install Homebrew"
+  open "https://brew.sh/index_ja"
   exit
 fi
