e7d58dcfeb337695814cccf7a1ddadda5dafd243
Shinya Oguri <36407060+shinyaoguri@users.noreply.github.com>
2022-08-09 12:33:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo hoge
+############
+# Xcodeの確認
+############
+echo -e "\nXcodeの存在確認"
+if type "xcode-select" >/dev/null 2>&1; then
+  echo -e "-> ✅ Xcode already exist"
+else
+  echo -e ">>> Xcode was not exists\n>>> Please install Xcode from AppStore."
+  return 2> /dev/null
+  exit
+fi
