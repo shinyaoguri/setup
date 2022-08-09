7dacb57ad4cb106b97fb1af2e8b27d0f419591d5
Shinya Oguri <36407060+shinyaoguri@users.noreply.github.com>
2022-08-09 17:36:34 +0900

    Update arm64_mac_setup.sh

--- arm64_mac_setup.sh
+++ arm64_mac_setup.sh
@@ -50,6 +50,6 @@ else
 fi
 
 cd ~
-zsh -c "$(curl -v -H 'Cache-Control: no-cache' -sfSL https://raw.githubusercontent.com/shinyaoguri/setup/main/ansible_arm64_mac.yml)"
+zsh -c "$(curl　-H 'Cache-Control: no-cache' -sfSL https://raw.githubusercontent.com/shinyaoguri/setup/main/ansible_arm64_mac.yml)"
 ansible-playbook -i hosts ansible_arm64_mac.yml --check
 rm ansible_arm64_mac.yml
