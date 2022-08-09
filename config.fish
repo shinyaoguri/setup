22d5280f5ce11e7a3cc028c60ea404cfb82c1b6c
shinyaoguri <dclab@dclab-mba.local>
2022-08-10 03:21:46 +0900

    anyenv

--- config.fish
+++ config.fish
@@ -1,1 +1,13 @@
+# M1チップのMacの場合のHomebrewの設定
+set -x PATH /opt/homebrew/bin $PATH
+set -x PATH /usr/local/bin $PATH
 
+#エイリアス
+alias rmt="trash -F"
+alias rm="rm -i"
+alias g="git"
+alias relogin="exec fish"
+alias reload="source ~/.config/fish/config.fish"
+alias t="tmuximum"
+alias '\;s'="ls"
+clear
