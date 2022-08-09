ca93274e26d686e5f59db69978e5fe9c37517a92
dclab <dclab@dclab-mba.local>
2022-08-10 02:38:48 +0900

    clone dotfile #1

--- arm64_mac_setup.sh
+++ arm64_mac_setup.sh
@@ -49,12 +49,14 @@ else
   brew install ansible
 fi
 
+
+
 echo -e "-----\nAnsible Deploy"
 cd ~
 curl -H "Cache-Control: no-cache" -O -sfSL https://raw.githubusercontent.com/shinyaoguri/setup/main/ansible_arm64_mac.yml
 
 if [ -f ~/ansible_arm64_mac.yml ]; then
-  ansible-playbook -i hosts ansible_arm64_mac.yml
+  ansible-playbook ansible_arm64_mac.yml --ask-become-pass
   rm ansible_arm64_mac.yml
 else
   echo -e "🙅 ansible-playbook was not downloaded"
