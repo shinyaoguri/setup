70006987c2c385fde1e31aac075949f1b00177ae
Shinya Oguri <36407060+shinyaoguri@users.noreply.github.com>
2022-10-09 00:29:17 +0900

    Update arm64_mac_setup.sh

--- arm64_mac_setup.sh
+++ arm64_mac_setup.sh
@@ -12,6 +12,7 @@ if type "xcode-select" >/dev/null 2>&1; then
   echo -e "✅ Xcode already exist"
 else
   echo -e "🙅 Xcode was not exist\n>>> Please install Xcode from AppStore."
+  open "https://apps.apple.com/jp/app/xcode/id497799835"
   return 2> /dev/null
   exit
 fi
@@ -54,9 +55,96 @@ else
 fi
 
 
-
 echo -e "-----\nAnsible Deploy"
 cd ~
+
+#メニュー
+options[0]="min"
+options[1]="max"
+
+#各メニューごとに挙動を記述する
+function DOIT {
+    if [[ ${choices[0]} ]]; then
+        echo "Option 1 selected"
+    fi
+
+    if [[ ${choices[1]} ]]; then
+        echo "Option 2 selected"
+    fi
+}
+
+clear
+
+#現在フォーカスしているメニュー
+current_line=0
+
+#メニュー一覧を表示
+function MENU {
+    echo "移動:[↑]or[↓], 選択:[SPACE], 決定:[ENTER]"
+    for NUM in ${!options[@]}; do
+        if [ $NUM -eq $current_line ]; then
+            echo -n ">"
+        else
+            echo -n " "
+        fi
+        echo "[${choices[NUM]:- }]"":${options[NUM]}"
+    done
+}
+
+
+#メニュー選択のループ
+function SELECT_LOOP {
+    while true; do
+        while MENU && IFS= read -r -n1 -s SELECTION && [[ -n "$SELECTION" ]]; do
+            if [[ $SELECTION == $'\x1b' ]]; then
+                read -r -n2 -s rest
+                SELECTION+="$rest"
+            fi  
+            clear
+
+            case $SELECTION in
+                $'\x1b\x5b\x41') #up arrow
+                    if [[ $current_line -ne 0 ]]; then
+                        current_line=$(( current_line - 1 ))
+                    else
+                        current_line=$(( ${#options[@]}-1 ))
+                    fi
+                    ;;
+                $'\x1b\x5b\x42') #down arrow
+                    if [[ $current_line -ne $(( ${#options[@]}-1 )) ]]; then
+                        current_line=$(( current_line + 1 ))
+                    else
+                        current_line=0
+                    fi
+                    ;;
+                $'\x20') #space
+                    if [[ "${choices[current_line]}" == "+" ]]; then
+                        choices[current_line]=""
+                    else
+                        choices[current_line]="+"
+                    fi
+                    ;;
+            esac
+        done
+
+        read -p "選べた? [Y/n]" Answer
+        case $Answer in
+            '' | [Yy]* )
+                break;
+                ;;
+            [Nn]* )
+                ;;
+            * )
+                echo "YES or NOで答えてね"
+                ;;
+        esac
+        clear
+    done
+}
+
+SELECT_LOOP
+DOIT
+
 curl -O -sfSL https://raw.githubusercontent.com/shinyaoguri/setup/main/ansible_arm64_mac.yml
 
 if [ -f ~/ansible_arm64_mac.yml ]; then
